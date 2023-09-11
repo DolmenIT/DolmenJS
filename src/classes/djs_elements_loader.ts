0a743eff69dd250b93306198a72d2073c809359c
DolmenIT <138483763+DolmenIT@users.noreply.github.com>
2023-09-11 21:13:03 +0000

    test

--- src/classes/djs_elements_loader.ts
+++ src/classes/djs_elements_loader.ts
@@ -4,29 +4,34 @@ export class djs_elements_loader {
     parameters = {};
 
     constructor() {
-        console.log("djs_elements_loader:constructor");
+      console.log("elements_loader:constructor");
     }
 
-    add = async (p_object: string) => {
-        try {
-            const response = await fetch("./elements/" + p_object + ".processed.js?" + Date.now());
-            const textResponse = await response.text();
-            eval(textResponse);
-        } catch (error) {
-            console.error("Erreur lors du chargement de l'élément :", error);
-        }
+    add = (p_object: string, p_var_struct) => {
+      try {
+        const response = djs.ni.loadJS("elements/"+p_object+".js", p_var_struct)
+        .then(response => response)
+        .then(js_response => {
+          if (typeof js_response.params !== "undefined") {
+            this.set_parameters(js_response.params)
+          }
+          eval(js_response.data);
+        })
+      } catch (error) {
+        console.error("Erreur lors du chargement de l'élément :", error);
+      }
     }
-
-    set_parameters(p_var_struct: any) {
-        if (typeof p_var_struct === 'object') {
-            this.parameters = p_var_struct; // On remplace les paramètres actuels par les nouveaux
-        } else {
-            console.error("Erreur : p_var_struct n'est pas un objet JSON valide.");
-        }
+  
+    set_parameters(p_var_struct) {
+      if (typeof p_var_struct === 'object') {
+        this.parameters = p_var_struct; // On remplace les paramètres actuels par les nouveaux
+      } else {
+        console.error("Erreur : p_var_struct n'est pas un objet JSON valide.");
+      }
     }
-
+  
     get_parameters() {
-        return this.parameters; // On renvoie les paramètres actuels
+      return this.parameters; // On renvoie les paramètres actuels
     }
 }
 //EOF
