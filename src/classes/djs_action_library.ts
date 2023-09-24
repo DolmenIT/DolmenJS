b57c49c6f4567e104cc6644e0f187773debb29e7
DolmenIT <138483763+DolmenIT@users.noreply.github.com>
2023-09-24 23:45:48 +0200

    2023_09_24

--- src/classes/djs_action_library.ts
+++ src/classes/djs_action_library.ts
@@ -7,7 +7,7 @@ export class djs_action_library {
     goto = (json_parameter: any) => {
         const scriptUrl = djs.rm.get(json_parameter.p_route);
         //const scriptUrl = new URL(document.currentScript.src).pathname;
-        const filename = scriptUrl.substring(scriptUrl.lastIndexOf('/') + 1);
+        //const filename = scriptUrl.substring(scriptUrl.lastIndexOf('/') + 1);
         if (typeof json_parameter.p_params != "undefined") {
             djs.params.setAll(json_parameter.p_params);
         }
